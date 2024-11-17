29870d3dfb117059b25d2a42579329213cfcb3c0
Omprakash Mourya. <105220456+Omprakashmourya143@users.noreply.github.com>
2024-11-17 17:37:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 // Set the port to listen on
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
